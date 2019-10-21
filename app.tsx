9cefcbd3a2d5a9406288376236e68572bfc38f3e
hesawant <hemant.sawant54@gmail.com>
2019-10-21 14:57:25 +0530

    Delete app.tsx

--- app.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-import "./app.less";
-
-export default class App extends React.Component {
-    render() {
-        return <div className="header">Hello World!</div>;
-    }
-}
